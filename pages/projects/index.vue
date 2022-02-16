dbda73428ff456fb0c7a802262580d9c7ea09e5f
Enok Madrid <enokslaboratory@gmail.com>
2022-02-15 19:44:31 -0800

    updated projects cards grid layout

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,3 +1,56 @@
 <template>
-    <SectionProjects />
+<section v-if="!loading" id="projects">
+    <section v-for="project in projects" 
+        :key="project.id" 
+        :style="`background-image: url(${project.imageHero.url})`"
+        class="container-fluid dark project--fullwidth" >
+            <article class="container">
+                <div class="row">
+                    <header class="col-lg-4 project--description">
+                        <ul class="project-tags mb-2">
+                            <li v-for="role in project.roles" :key="role.id">{{role.title}} &middot;</li>
+                        </ul>
+                        <h3 class="project-title h1 mb-4">{{project.title}}</h3>
+                        <p class="project-text">
+                            {{project.clientDescription}}
+                        </p>
+                        <NuxtLink 
+                            :to="`/projects/${project.slug}`"
+                            :key="project.slug"
+                            class="btn btn-lg btn-light rounded-pill" 
+                            target="blank"
+                            >
+                            View Case Study
+                        </NuxtLink>
+                    </header>
+                    <aside class="col-lg-8 col-lg-push-1 no-gutter project--image">
+                        <ProjectImage :src="`${project.imageScreen.url}`" class="img-fluid" />
+                    </aside>
+                </div>
+            </article>
+        </section>
+</section>
 </template>
+
+<script>
+import projectsQuery from '~/graphql/projects.gql'
+export default {
+
+    data: () => ({
+        loading: 0,
+        projects: [],
+        projectNext: '',
+        projectPrevious: ''
+    }),
+    apollo: {
+        $loadingKey: 'loading',
+        projects: {
+            prefetch: true,
+            query: projectsQuery,
+            variables() {
+             return { slug: this.$route.params.slug }
+            }
+        }
+    }
+}
+</script>
