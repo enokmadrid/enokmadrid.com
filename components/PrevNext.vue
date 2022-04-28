00602671a7bf2c2958e6f47267fc1ed0569dcd9b
Enok Madrid <enokslaboratory@gmail.com>
2022-04-27 18:26:21 -0700

    fix the Prev Next links to use the button component

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -1,27 +1,27 @@
 <template>
 	<div class="flexbox-container content--menu">
-		<nuxt-link 
+		<Button
 			v-if="prev" 
 			:to="`${prev.path}`" 
 			class="flex-link btn btn-link has-arrow __left">
 				<span>{{prev.title}}</span>
-		</nuxt-link>
-		<nuxt-link 
+		</Button>
+		<Button
 			v-if="next" 
 			:to="`${next.path}`" 
 			class="flex-link btn btn-link has-arrow __right"
 			>
 			<span>{{next.title}}</span>
-		</nuxt-link>
+		</Button>
 	</div>
 </template>
 
 <script>
 export default {
-	props: {
-		prev: {},
-		next: {}
-	}
+    props: {
+        prev: {},
+        next: {}
+    }
 }
 </script>
 
