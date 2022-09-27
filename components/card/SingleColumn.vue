f78f76b895467447a7968c37e3e0738bdbb7fef3
Enok Madrid <enokslaboratory@gmail.com>
2022-09-26 18:17:49 -0700

    styled blog cards + tag theming

--- components/card/SingleColumn.vue
+++ components/card/SingleColumn.vue
@@ -1,13 +1,13 @@
 <template>
-    <a :class="['row mb-5']" :href="articleUrl">
+    <a :class="['blog-card', 'row mb-5']" :href="articleUrl">
         <div class="col-6">
             <img :src="imageUrl" class="article-img img-fluid" alt="Responsive image">
         </div>
-        <div class="col-6">
-            <span>{{tag}}</span>
-            <h3>{{headline}}</h3>
+        <div class="col-6 d-inline-flex flex-column align-items-start justify-content-center">
+            <tag :title="tag" :theme="tagTheme"></tag>
+            <h3 class="headline">{{headline}}</h3>
             <p>{{description}}</p>
-            <time>{{updatedAt}}</time>
+            <time class="time">{{updatedAt}}</time>
         </div>
     </a>
 </template>
@@ -18,6 +18,7 @@
             "articleUrl",
             "imageUrl",
             "tag",
+            "tagTheme",
             "headline",
             "description",
             "updatedAt",
@@ -30,5 +31,27 @@
 
 .article-img {
     border-radius: 8px;
+}
+.blog-card {
+    color: unset;
+    &:hover {
+        color: unset;
+        text-decoration: none;
+    }
+    .headline {
+        font-family: "Proxima Nova";
+        font-weight: 800;
+        font-size: $space-standard;
+        text-transform: none;
+        margin-bottom: $space-standard;
+    }
+    .time {
+        font-size: 11px;
+        font-weight: 700;
+    }
+    .headline,
+    .decription {
+        margin-bottom: $space-standard;
+    }
 }
 </style>
--- components/card/Standard.vue
+++ components/card/Standard.vue
@@ -1,13 +1,11 @@
 <template>
-    <a class="col-3" :href="articleUrl">
-        <header>
-            <img :src="imageUrl" class="article-img img-fluid" alt="Responsive image">
-        </header>
-        <article class="card-body">
-            <span>{{tag}}</span>
-            <h3>{{headline}}</h3>
+    <a class="col-4 blog-card" :href="articleUrl">
+        <img :src="imageUrl" class="article-img img-fluid" alt="Responsive image">
+        <article>
+            <tag :title="tag" :theme="tagTheme"></tag>
+            <h3 class="headline">{{headline}}</h3>
             <p>{{description}}</p>
-            <time>{{updatedAt}}</time>
+            <time class="time">{{updatedAt}}</time>
         </article>
     </a>
 </template>
@@ -18,10 +16,11 @@
             "articleUrl",
             "imageUrl",
             "tag",
+            "tagTheme",
             "headline",
             "description",
             "updatedAt",
-        ],
+        ]
     }
 </script>
 
@@ -29,6 +28,29 @@
 @import '~/assets/scss/_mixins.scss';
 
 .article-img {
-    border-radius: 8px;
+    border-radius: $space-tiny;
+    margin-bottom: $space-small;
+}
+.blog-card {
+    color: unset;
+    &:hover {
+        color: unset;
+        text-decoration: none;
+    }
+    .headline {
+        font-family: "Proxima Nova";
+        font-weight: 800;
+        font-size: $space-standard;
+        text-transform: none;
+        margin-bottom: $space-standard;
+    }
+    .time {
+        font-size: 11px;
+        font-weight: 700;
+    }
+    .headline,
+    .decription {
+        margin-bottom: $space-standard;
+    }
 }
 </style>
