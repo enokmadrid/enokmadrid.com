5d0cecd08da2395fc2e422dd7da8f9d2c9d06c3f
Enok Madrid <enokslaboratory@gmail.com>
2024-05-14 13:45:35 -0700

    added wow robot

--- components/home/PageBox.vue
+++ components/home/PageBox.vue
@@ -2,12 +2,10 @@
 	<section id="solutions" class="container-fluid background-gradient section-height">
 		<div class="p-relative z-5">
 			<Title class="py-5"
-				data-aos="fade-up" 
-				data-aos-duration="500"
-				data-aos-easing="ease-in-out-cubic"
 				:title="`Design & Code`"
 				:subtitle="`I can help you with designing and building experiences with simple content structure, clean UI patterns with a slight touch of wow.`"
 			/>
+			<canvas id="canvas" width="100" height="100"></canvas>
 			<div class="container"
 				data-aos="fade-up" 
 				data-aos-duration="500"
@@ -41,15 +39,36 @@
 
 <script>
 import aosMixin from '~/mixins/aos'
+import { Rive } from "@rive-app/canvas";
+
 export default {
 	data: () => ({
-		cards: []
+		cards: [],
+		animation: null
 	}),
 	async created() {
 		this.cards = await this.$content('iconCards')
 		.sortBy('id')
 		.limit(3)
 		.fetch();
+	},
+	mounted() {
+		this.initializeRive();
+	},
+	methods: {
+		initializeRive() {
+			if (process.browser) {
+				this.animation = new Rive({
+					src: "/rive/wow_robot.riv",
+					canvas: document.getElementById("canvas"),
+					autoplay: true,
+					stateMachines: "State Machine 1",
+					onLoad: () => {
+						this.animation.resizeDrawingSurfaceToCanvas();
+					},
+				});
+			}
+		}
 	},
 	mixins: [aosMixin]
 }
@@ -92,5 +111,16 @@ $section-height: 1150px;
 .brand-pattern-bg {
 	left: 730px;
 	border-top: 5px solid $primary;
+}
+#canvas {
+	height: 100px;
+    width: 100px;
+    position: relative;
+    top: -150px;
+    left: 250px;
+    margin: 0 auto;
+    text-align: center;
+    display: block;
+	cursor: pointer;
 }
 </style>
